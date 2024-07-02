1973b6dbb6407ade7b046eca976dc7186d302ebf
CharlyGoros <111509721+CharlyGoros@users.noreply.github.com>
2024-07-02 01:33:13 -0300

    added: pantalla login e incio metodo auth
    a mimir

---
+++ app/src/main/java/com/example/notesapp/LoginActivity.java
@@ -0,0 +1,87 @@
+package com.example.notesapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText emailEditText,passwordEditText,confirmPasswordEditText;
+    Button LoginBtn;
+    ProgressBar progressBar;
+    TextView createAccountBtnTextView;
+    private View loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        loginBtn = findViewById(R.id.login_btn);
+        progressBar = findViewById(R.id.progress_bar);
+        createAccountBtnTextView = findViewById(R.id.create_account_text_view_btn);
+
+
+        loginBtn.setOnClickListener((v)-> loginUser() );
+        createAccountBtnTextView.setOnClickListener((v)->startActivity(new Intent(LoginActivity.this,CreateAccountActivity.class)) );
+
+        void loginUser(){
+            String email  = emailEditText.getText().toString();
+            String password  = passwordEditText.getText().toString();
+
+
+            boolean isValidated = validateData(email,password);
+            if(!isValidated){
+                return;
+            }
+            loginAccountInFirebase(email,password);
+
+        }
+
+
+    }
+
+    void loginAccountInFirebase(String email,String password){
+      
+    }
+
+
+
+    void changeInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            loginBtn.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            loginBtn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    boolean validateData(String email,String password){
+        //validate the data that are input by user.
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("Email is invalid");
+            return false;
+        }
+        if(password.length()<6){
+            passwordEditText.setError("Password length is invalid");
+            return false;
+        }
+
+        return true;
+    }
+}
