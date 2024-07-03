f55c586631429d9b5cc83abe0d232bdeff89c5d9
CharlyGoros <111509721+CharlyGoros@users.noreply.github.com>
2024-07-03 01:00:27 -0300

    added: preps p recycler view, falta adapter y demas

--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -2,18 +2,24 @@ package com.example.notesapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+
+        //bajar de glide el logo de login y sign up para el final!! (colsutar igna)
     FloatingActionButton addNoteBtn;
+    RecyclerView recyclerView;
+    ImageButton menuBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +27,21 @@ public class MainActivity extends AppCompatActivity {
 
 
         addNoteBtn = findViewById(R.id.add_note_btn);
-
+        recyclerView = findViewById(R.id.recyler_view);
+        menuBtn = findViewById(R.id.menu_btn);
         addNoteBtn.setOnClickListener((v)-> startActivity(new Intent(MainActivity.this,NoteDetsActivity.class)) );
+        menuBtn.setOnClickListener((v)->showMenu() );
+        setupRecyclerView();
+    }
+
+
+    void showMenu(){
+        //menu hamburguesa en teoria
+    }
+
+
+    void setupRecyclerView(){
+
     }
 }
 
